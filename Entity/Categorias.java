2a611df4534f12a7984d8f6fd99fed7281973fe9
José Néstor Meneses García <62455290+jnestormg@users.noreply.github.com>
2024-01-29 20:49:32 -0600

    cambio en entidades

---
+++ Entity/Categorias.java
@@ -0,0 +1,34 @@
+package Entity;
+
+import java.util.List;
+
+public class Categorias {
+    private String nombreCategoria;
+    
+    private List<Productos> listaProductos;
+
+    public Categorias(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public List<Productos> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<Productos> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    
+
+    
+
+}
