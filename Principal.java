eaab687bdde8895973c738c01e3b6c2b86a5ed2e
José Néstor Meneses García <62455290+jnestormg@users.noreply.github.com>
2024-01-29 21:07:08 -0600

    cambio en principal

--- Principal.java
+++ Principal.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import Entity.Catalogos;
 import Entity.Menus;
 import Entity.Productos;
 
@@ -26,13 +25,13 @@ public class Principal {
     private static String nombreMenu;
 
     public static void main(String[] args) {
-        System.out.println("Menu");
         Scanner leerDato = new Scanner(System.in);
         int opcion;
         do {
             String opcionesMenu = "1. Crear Menú." +
-                    "2. Crear Catalogo."+
-                    "3. Agregar Producto.";
+                    "\n2. Crear Catalogo."+
+                    "\n3. Agregar Producto.";
+                    System.out.println(opcionesMenu);
             opcion = leerDato.nextInt();
             switch (opcion) {
                 case 1:
@@ -45,47 +44,9 @@ public class Principal {
             }
 
         } while (opcion != 3);
-        agregarNombreMenu();
-        agregarCatalogo();
-        agregarProductos();
-
+       leerDato.close();
     }
 
-    public static void agregarNombreMenu() {
-        Scanner leerDato = new Scanner(System.in);
-        System.out.println("Nombre del menú");
-        nombreMenu = leerDato.nextLine();
-        menu = new Menus(nombreMenu);
-    }
-
-    public static void agregarCatalogo() {
-        Scanner leerDato = new Scanner(System.in);
-        System.out.println("Nombre del catalogo");
-        nombreCatalogo = leerDato.nextLine();
-        catalogo = new Catalogos(nombreCatalogo);
-    }
-
-    public static void agregarProductos() {
-        Scanner leerDato = new Scanner(System.in);
-        System.out.print("Nombre producto:");
-        nombreProducto = leerDato.nextLine();
-        System.out.print("Precio del producto:");
-        precioProducto = leerDato.nextFloat();
-        listarProductos(nombreProducto, precioProducto);
-    }
-
-    public static List<Productos> listarProductos(String product, Float precio) {
-        producto = new Productos(product, precio);
-        productos = new ArrayList<>();
-        productos.add(producto);
-        return productos;
-    }
-
-    public static void mostrarProductos(List<Productos> productos) {
-        productos.stream().forEach((x) -> {
-            System.out.println("Producto: " + x.getNombreProducto());
-            System.out.println("Precio: " + x.getPrecioProducto());
-        });
-    }
+    
 
 }
